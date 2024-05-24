f752d47b63dfbf1dd33dce6a9ad26323f8673518
HJYao <119796101+HJYao00@users.noreply.github.com>
2024-05-25 00:12:28 +0800

    Add files via upload

---
+++ scripts/v1_5/eval/video/run_benchmark_generic_qa.sh
@@ -0,0 +1,40 @@
+CKPT_NAME="DenseConnector-v1.5-7B"
+num_frames=8
+model_path="./ckpt/DenseConnector-v1.5-7B"
+GPT_Zero_Shot_QA="./path_to_data/GPT_Zero_Shot_QA"
+video_dir="${GPT_Zero_Shot_QA}/VideoChatGPT_Test_Videos"
+gt_file="${GPT_Zero_Shot_QA}/generic_qa.json"
+output_dir="output/Benchmark_Generic_QA/${CKPT_NAME}_u${num_frames}FRS_Tcat_tk512_tem0_pool"
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+  CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python3 dc/eval/run_inference_benchmark_general.py \
+      --video_dir ${video_dir} \
+      --gt_file ${gt_file} \
+      --output_dir ${output_dir} \
+      --output_name ${CHUNKS}_${IDX} \
+      --model_name ${model_path} \
+      --num_chunks $CHUNKS \
+      --num_frames $num_frames \
+      --conv-mode vicuna_v1 \
+      --temperature 0 \
+      --use_pool \
+      --chunk_idx $IDX &
+done
+
+wait
+
+output_file=${output_dir}/generic.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ${output_dir}/${CHUNKS}_${IDX}.json >> "$output_file"
+done
---
+++ scripts/v1_5/eval/video/run_qa_anet_uniform.sh
@@ -0,0 +1,42 @@
+CKPT_NAME="DenseConnector-v1.5-7B"
+num_frames=8
+model_path="./ckpt/DenseConnector-v1.5-7B"
+GPT_Zero_Shot_QA="./path_to_data/GPT_Zero_Shot_QA"
+video_dir="${GPT_Zero_Shot_QA}/Activitynet_Zero_Shot_QA/all_test"
+gt_file_question="${GPT_Zero_Shot_QA}/Activitynet_Zero_Shot_QA/test_q.json"
+gt_file_answers="${GPT_Zero_Shot_QA}/Activitynet_Zero_Shot_QA/test_a.json"
+output_dir="output/Activitynet_Zero_Shot_QA/${CKPT_NAME}_u${num_frames}FRS_pool"
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+  CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python3 dc/eval/run_inference_qa.py \
+      --video_dir ${video_dir} \
+      --gt_file_question ${gt_file_question} \
+      --gt_file_answers ${gt_file_answers} \
+      --output_dir ${output_dir} \
+      --output_name ${CHUNKS}_${IDX} \
+      --model_name ${model_path} \
+      --num_chunks $CHUNKS \
+      --num_frames $num_frames \
+      --conv-mode vicuna_v1 \
+      --temperature 0 \
+      --use_pool \
+      --chunk_idx $IDX &
+done
+
+wait
+
+output_file=${output_dir}/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ${output_dir}/${CHUNKS}_${IDX}.json >> "$output_file"
+done

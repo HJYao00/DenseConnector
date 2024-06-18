0a9186a8799f11b1647e3f121119f3a4e371b342
HJYao <917822602@qq.com>
2024-06-18 19:20:34 +0800

    Update clip_encoder.py

--- dc/model/multimodal_encoder/clip_encoder.py
+++ dc/model/multimodal_encoder/clip_encoder.py
@@ -77,8 +77,8 @@ class CLIPVisionTower(nn.Module):
         return self.config.hidden_size
         
     @property
-    def num_patches(self):
-        return (self.config.image_size // self.config.patch_size) ** 2
+    def num_patches_per_side(self):
+        return self.config.image_size // self.config.patch_size
 
     @property
     def num_patches(self):
